a6a44b07879d26d987cc2b85afe67b8ac715690e
MakerSquare Student <student@lab-5.att.net>
2013-06-20 11:35:30 -0500

    added another great idea

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,6 @@ Ideator::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'ideas#index'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
